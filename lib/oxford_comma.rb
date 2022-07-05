e7fd3acb2746f250056fd22a96ba2125c24b31c5
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-07-05 13:28:10 -0400

    commit tree 91fc984b614a330770aaa3c543f858a6eb7e7bd1

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(items)
+    if items.length == 1
+        items.join (" ")
+    elsif items.length == 2
+        items.join (" and ")
+    elsif items.length > 2
+        items[-1] = "and " + items[-1]
+        puts items
+        items[0..-1].join(", ")
+    end
 end
